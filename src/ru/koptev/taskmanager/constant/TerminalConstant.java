56a1c527bde0d146bc6a2e7500fbcd5600a6fb9b
Koptev_Anton <koptev_av@nlmk.com>
2020-07-08 20:28:55 +0300

    Добавлен абзац в TerminalConstant

--- src/ru/koptev/taskmanager/constant/TerminalConstant.java
+++ src/ru/koptev/taskmanager/constant/TerminalConstant.java
@@ -1,6 +1,7 @@
 package ru.koptev.taskmanager.constant;
 
 public class TerminalConstant {
+
     public static final String ABOUT = "about";
     public static final String HELP = "help";
     public static final String VERSION = "version";
