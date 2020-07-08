66cbf27f74b4ccf6cd01bd8b9ece22ac078649f4
Koptev_Anton <koptev_av@nlmk.com>
2020-07-08 20:24:13 +0300

    Добавлена обработка аргументов командной строки со значениями, version, about, help и обработка некорректных параметров. Добавлен класс TerminalConstant c константами.

--- src/ru/koptev/taskmanager/Main.java
+++ src/ru/koptev/taskmanager/Main.java
@@ -1,8 +1,51 @@
 package ru.koptev.taskmanager;
 
+import static ru.koptev.taskmanager.constant.TerminalConstant.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        displayWelcome();
+        run(args);
     }
+
+    public static void run(String[] args) {
+        if (args == null) return;
+        if (args.length < 1) return;
+        String param = args[0];
+        switch (param) {
+            case (HELP): displayHelp();
+            case (ABOUT): displayAbout();
+            case (VERSION): displayVersion();
+            default: displayError();
+        }
+    }
+
+    public static void displayHelp() {
+        System.out.println("about - Display developer information.");
+        System.out.println("version - Display program version.");
+        System.out.println("help - Display list of terminal commands.");
+        System.exit(0);
+    }
+
+    public static void displayAbout() {
+        System.out.println("Name: Koptev Anton");
+        System.out.println("Email: koptev_av@nlmk.com");
+        System.exit(0);
+    }
+
+    public static void displayVersion() {
+        System.out.println("Version: 1.0.0");
+        System.exit(0);
+    }
+
+    public static void displayError(){
+        System.out.println("Error! Invalid parameter...");
+        System.exit(-1);
+    }
+
+    public static void displayWelcome(){
+        System.out.println("** WELCOME TO TASK MANAGER **");
+    }
+
 }
---
+++ src/ru/koptev/taskmanager/constant/TerminalConstant.java
@@ -0,0 +1,8 @@
+package ru.koptev.taskmanager.constant;
+
+public class TerminalConstant {
+    public static final String ABOUT = "about";
+    public static final String HELP = "help";
+    public static final String VERSION = "version";
+
+}
