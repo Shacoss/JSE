ddd8f3dfcb573b9054ccf9456ac1ef03c58d4a3d
Koptev_Anton <koptev_av@nlmk.com>
2020-07-10 20:24:31 +0300

    Добавлен класс MainTest в директорию тестирования.

---
+++ src/test/java/ru/koptev/taskmanager/MainTest.java
@@ -0,0 +1,15 @@
+package ru.koptev.taskmanager;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class MainTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+}
